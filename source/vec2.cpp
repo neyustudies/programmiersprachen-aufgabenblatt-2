28d6bd2adcd4302e8a2e94c7301e20415f8d5d6b
neyustudies <science-cat@gmx.net>
2020-05-16 10:47:10 +0200

    Add operators definition for Vec2

--- source/vec2.cpp
+++ source/vec2.cpp
@@ -6,3 +6,37 @@
 */
 
 #include "vec2.hpp"
+
+//default constructor
+Vec2::Vec2():
+  x_{0.0f}, 
+  y_{0.0f} {}
+
+// value constructor
+Vec2::Vec2 (float x, float y):
+  x_{x},
+  y_{y} {}
+
+// definition addition for Vec2
+Vec2& Vec2::operator+=(Vec2 const& v) {
+  x_ += v.x_;
+  y_ += v.y_;
+}
+
+// definition multiplication for Vec2
+Vec2& Vec2::operator*=(float s) {
+  x_ *= s;
+  y_ *= s;
+}
+
+// definition subtraction for Vec2
+Vec2& Vec2::operator-=(Vec2 const& v) {
+  x_ -= v.x_;
+  y_ -= v.y_;
+}
+
+// definition division for Vec2
+Vec2& Vec2::operator/=(float s) {
+  x_ /= s;
+  y_ /= s;
+}
