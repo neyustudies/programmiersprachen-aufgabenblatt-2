1008eff9cb7d33c71e28f81666a158bca224afb1
neyustudies <science-cat@gmx.net>
2020-05-16 12:31:07 +0200

    Add expression to return objects in vec2.cpp

--- source/vec2.cpp
+++ source/vec2.cpp
@@ -12,7 +12,7 @@ Vec2::Vec2():
   x_{0.0f}, 
   y_{0.0f} {}
 
-// value constructor
+// var constructor
 Vec2::Vec2 (float x, float y):
   x_{x},
   y_{y} {}
@@ -21,22 +21,26 @@ Vec2::Vec2 (float x, float y):
 Vec2& Vec2::operator+=(Vec2 const& v) {
   x_ += v.x_;
   y_ += v.y_;
+  return *this;
 }
 
 // definition multiplication for Vec2
 Vec2& Vec2::operator*=(float s) {
   x_ *= s;
   y_ *= s;
+  return *this;
 }
 
 // definition subtraction for Vec2
 Vec2& Vec2::operator-=(Vec2 const& v) {
   x_ -= v.x_;
   y_ -= v.y_;
+  return *this;
 }
 
 // definition division for Vec2
 Vec2& Vec2::operator/=(float s) {
   x_ /= s;
   y_ /= s;
+  return *this;
 }
