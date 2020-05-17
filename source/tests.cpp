c6083c5b6d8e1ff7173e3b1722b71acfb858a5f6
neyustudies <science-cat@gmx.net>
2020-05-17 16:05:37 +0200

    const correctness in tests.cpp

--- source/tests.cpp
+++ source/tests.cpp
@@ -21,9 +21,9 @@
 
 // testing initialization of instances
 TEST_CASE("describe_vec2", "[vec2]") {
-  Vec2 a;
-  Vec2 b{5.1f, -9.3f};
-  Vec2 c(-1.0f, 2.0f);
+  const Vec2 a;
+  const Vec2 b{5.1f, -9.3f};
+  const Vec2 c(-1.0f, 2.0f);
 
   REQUIRE(a.x == Approx(0.0f));
   REQUIRE(a.y == Approx(0.0f));
@@ -38,7 +38,7 @@ TEST_CASE("describe_vec2", "[vec2]") {
 TEST_CASE("describe_addition_vec2", "[addition vec2]") {
   Vec2 v1;
   Vec2 v2{4.3f, 1.5f};
-  Vec2 v3{3.0f, -2.7f};
+  const Vec2 v3{3.0f, -2.7f};
 
   v1+=v2;
   v2+=v3;
@@ -55,7 +55,7 @@ TEST_CASE("describe_multiplication_vec2", "[multiplication vec2]") {
   Vec2 v1;
   Vec2 v2{2.6f, 4.0f};
   Vec2 v3{-2.8f, 0.3f};
-  float s = 2;
+  const float s = 2;
 
   v1*=s;
   v2*=s;
@@ -74,7 +74,7 @@ TEST_CASE("describe_multiplication_vec2", "[multiplication vec2]") {
 TEST_CASE("describe_substraction_vec2", "[subtraction vec2]") {
   Vec2 v1{3.0f, 1.3f};
   Vec2 v2;
-  Vec2 v3{6.3f, -4.7f};
+  const Vec2 v3{6.3f, -4.7f};
 
   v1-=v2;
   v2-=v3;
@@ -91,7 +91,7 @@ TEST_CASE("describe_division_vec2", "[division vec2]") {
   Vec2 v1{8.0f, 4.6f};
   Vec2 v2{2.0f, 1.3f};
   Vec2 v3{1.1f, -3.3f};
-  float s = 2;
+  const float s = 2;
 
   v1/=s;
   v2/=s;
@@ -112,8 +112,8 @@ TEST_CASE("describe_division_vec2", "[division vec2]") {
 
 // testing initialization of instances
 TEST_CASE("describe_mat2", "[mat2]") {
-  Mat2 m1;
-  Mat2 m2{2.5f, 1.5f, 1.0f, 2.0f};
+  const Mat2 m1;
+  const Mat2 m2{2.5f, 1.5f, 1.0f, 2.0f};
 
   REQUIRE(m1.e_00 == Approx(1.0f));
   REQUIRE(m1.e_01 == Approx(0.0f));
@@ -130,11 +130,11 @@ TEST_CASE("describe_mat2", "[mat2]") {
 TEST_CASE("describe_operator*=", "[multiplication mat2]") {
   Mat2 m1;
   Mat2 m2{2.5f, -4.0f, 1.2f, 3.0f};
-  Mat2 m3{0.0f, 0.0f, -1.0f, 0.2f};
+  const Mat2 m3{0.0f, 0.0f, -1.0f, 0.2f};
 
   m1*=m2;
   m2*=m3;
-  Mat2 m4 = m1 * m2; 
+  const Mat2 m4 = m1 * m2; 
 
   REQUIRE(m1.e_00 == Approx(2.5f));
   REQUIRE(m1.e_01 == Approx(-4.0f));
@@ -153,9 +153,9 @@ TEST_CASE("describe_operator*=", "[multiplication mat2]") {
 
 // testing determinant of mat2
 TEST_CASE("describe_determinant", "[determinant mat2]") {
-  Mat2 m1;
-  Mat2 m2{2.0f, 6.0f, 8.0f, 3.0f};
-  Mat2 m3{4.0f, 6.3f, 0.4f, 2.0f};
+  const Mat2 m1;
+  const Mat2 m2{2.0f, 6.0f, 8.0f, 3.0f};
+  const Mat2 m3{4.0f, 6.3f, 0.4f, 2.0f};
 
   REQUIRE(m1.det() == Approx(1.0f));
   REQUIRE(m2.det() == Approx(-42.0f));
@@ -170,13 +170,13 @@ TEST_CASE("describe_determinant", "[determinant mat2]") {
 
 // testing operator* of mat2
 TEST_CASE("describe_operator*", "[free multiplication mat2]") {
-  Mat2 m1;
-  Mat2 m2{2.5f, -4.0f, 1.2f, 3.0f};
-  Mat2 m3{0.0f, 0.0f, -1.0f, 0.2f};
+  const Mat2 m1;
+  const Mat2 m2{2.5f, -4.0f, 1.2f, 3.0f};
+  const Mat2 m3{0.0f, 0.0f, -1.0f, 0.2f};
 
-  Mat2 m4 = m1 * m2;
-  Mat2 m5 = m2 * m3;
-  Mat2 m6 = m4 * m5;
+  const Mat2 m4 = m1 * m2;
+  const Mat2 m5 = m2 * m3;
+  const Mat2 m6 = m4 * m5;
 
   REQUIRE(m4.e_00 == Approx(2.5f));
   REQUIRE(m4.e_01 == Approx(-4.0f));
@@ -195,9 +195,9 @@ TEST_CASE("describe_operator*", "[free multiplication mat2]") {
 
 // testing operator* of matrix and vector and vice versa
 TEST_CASE("describe_free_operator*_mat_vec", "[multiplication mat2 vec2]") {
-  Mat2 m1;
+  const Mat2 m1;
+  const Mat2 m2{2.5f, -4.0f, 1.2f, 3.0f};
   Vec2 v1;
-  Mat2 m2{2.5f, -4.0f, 1.2f, 3.0f};
   Vec2 v2{5.1f, -9.3f};
 
   v1 = m1 * v1;
@@ -212,10 +212,10 @@ TEST_CASE("describe_free_operator*_mat_vec", "[multiplication mat2 vec2]") {
 
 // testing inverse of mat2
 TEST_CASE("describe_inverse", "[inverse mat2]") {
-  Mat2 m0;
-  Mat2 m1{3.0f, 5.0f, 3.5f, 6.0f};
-  Mat2 m2 = inverse(m0);
-  Mat2 m3 = inverse(m1);
+  const Mat2 m0;
+  const Mat2 m1{3.0f, 5.0f, 3.5f, 6.0f};
+  const Mat2 m2 = inverse(m0);
+  const Mat2 m3 = inverse(m1);
 
   REQUIRE(m2.e_00 == Approx(1.0f));
   REQUIRE(m2.e_01 == Approx(0.0f));
@@ -229,10 +229,10 @@ TEST_CASE("describe_inverse", "[inverse mat2]") {
 
 // testing transpose of mat2
 TEST_CASE("describe_transpose", "[transpose mat2]") {
-  Mat2 m0;
-  Mat2 m1{1.0f, 2.0f, 3.0f, 4.0f};
-  Mat2 m2 = transpose(m0);
-  Mat2 m3 = transpose(m1);
+  const Mat2 m0;
+  const Mat2 m1{1.0f, 2.0f, 3.0f, 4.0f};
+  const Mat2 m2 = transpose(m0);
+  const Mat2 m3 = transpose(m1);
 
   REQUIRE(m2.e_00 == Approx(1.0f));
   REQUIRE(m2.e_01 == Approx(0.0f));
@@ -247,8 +247,8 @@ TEST_CASE("describe_transpose", "[transpose mat2]") {
 
 // testing rotation of mat2
 TEST_CASE("describe_rotate", "[rotate mat2]") {
-  Mat2 m1 = make_rotation_mat2(90.0f);
-  Mat2 m2 = make_rotation_mat2(45.0f);
+  const Mat2 m1 = make_rotation_mat2(90.0f);
+  const Mat2 m2 = make_rotation_mat2(45.0f);
 
   REQUIRE(m1.e_00 == Approx(-0.44807f));
   REQUIRE(m1.e_01 == Approx(-0.894f));
