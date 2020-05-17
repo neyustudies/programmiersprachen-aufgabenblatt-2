28a4a6c4aca36d4da5e936c3419a08a9280653be
neyustudies <science-cat@gmx.net>
2020-05-17 17:13:29 +0200

    Create color.hpp

---
+++ source/color.hpp
@@ -0,0 +1,18 @@
+/**
+ * Programmiersprachen SoSe 2020
+ * Aufgabensammlung 2
+ * Lisa P.
+ * color.hpp
+*/
+
+#ifndef COLOR_HPP
+#define COLOR_HPP
+
+struct Color {
+
+  // TODO: member variables
+
+
+};
+
+#endif // COLOR_HPP
