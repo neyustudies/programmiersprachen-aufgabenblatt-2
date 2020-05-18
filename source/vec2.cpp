cb93670463396683b261e1f7828b48795817f74d
neyustudies <science-cat@gmx.net>
2020-05-18 17:21:44 +0200

    add free functions for vec2 in vec2.cpp

--- source/vec2.cpp
+++ source/vec2.cpp
@@ -34,3 +34,27 @@ Vec2& Vec2::operator/=(float s) {
   y /= s;
   return *this;
 }
+
+/*--- Free Functions of vec2 -------------------------*/
+
+Vec2 operator+(Vec2 const& u, Vec2 const& v) {
+  return Vec2{u.x + v.x, u.y + v.y};
+}
+
+Vec2 operator-(Vec2 const& u, Vec2 const& v) {
+  return Vec2{u.x - v.x, u.y - v.y};
+}
+
+Vec2 operator*(Vec2 const& v, float s) {
+  return Vec2{v.x * s, v.y * s};
+}
+
+Vec2 operator/(Vec2 const& v, float s) {
+  if (s == 0) {
+    return v;
+  } return Vec2{v.x / s, v.y / s};
+}
+
+Vec2 operator*(float s, Vec2 const& v) {
+  return v * s;
+}
