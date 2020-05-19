fe5f6a84e0de6a7fc1e2315e911cac11fa2fc11d
neyustudies <science-cat@gmx.net>
2020-05-19 17:50:56 +0200

    add draw to circle.hpp and circle.cpp (aufg. 2.11)

--- source/circle.cpp
+++ source/circle.cpp
@@ -15,6 +15,12 @@ Circle::Circle(Vec2 const& ctr, float r) {
   radius_ = r;
 }
 
+Circle::Circle(Vec2 const& ctr, float r, Color const& col) {
+  center_ = ctr;
+  radius_ = r;
+  color_  = col;
+}
+
 // area: pi * r^2
 float Circle::area() const {
   return M_PI * pow(radius_, 2);
@@ -35,5 +41,13 @@ Vec2 Circle::center() const {
 
 Color Circle::color() const {
   return color_;
+}
+
+void Circle::draw(Window const& win) const {
+  for(int i = 0; i <= 500; i++) {
+    Vec2 start = make_rotation_mat2((2 * M_PI)/500 * i) * Vec2{radius_, 0} + center_;
+    Vec2 end = make_rotation_mat2((2 * M_PI)/500 * (i+1)) * Vec2{radius_, 0} + center_;
+    win.draw_line(start.x, start.y, end.x, end.y, color_.r, color_.g, color_.b);  
+  }
 }
 
--- source/circle.hpp
+++ source/circle.hpp
@@ -10,15 +10,19 @@
 
 #include "vec2.hpp"
 #include "color.hpp"
+#include "window.hpp"
 
 class Circle {
   public:
     Circle(Vec2 const& ctr, float r);
+    Circle(Vec2 const& ctr, float r, Color const& clr);
     float area() const;
     float circumference() const;
     float radius() const;
     Vec2 center() const;
     Color color() const;
+    void draw(Window const& win) const;
+    void draw(Window const& win, Color const& clr) const;
 
   private:
     Vec2 center_;
