ae43601ea9ba8eed7bedf19ba82d8773a299a6a0
neyustudies <science-cat@gmx.net>
2020-05-19 11:58:09 +0200

    add color to rectangle.hpp

--- source/rectangle.hpp
+++ source/rectangle.hpp
@@ -9,21 +9,26 @@
 #define RECTANGLE_HPP
 
 #include "vec2.hpp"
+#include "color.hpp"
 
 
 class Rectangle {
 
   public:
     Rectangle(Vec2 const& min, Vec2 const& max);
+    Rectangle(Vec2 const& min, Vec2 const& max, Color const& col);
     float area() const;
     float circumference() const;
+    float width() const;
+    float height() const;
     Vec2 max() const;
     Vec2 min() const;
 
   private:
     Vec2 max_;
     Vec2 min_;
-    
+    Color color_;
+
 };
 
 #endif // RECTANGLE_HPP
